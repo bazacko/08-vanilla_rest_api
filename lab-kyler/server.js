146fc8e2ad5b976b40bc60dbc23d30699244176a
Kyler Dotson <kyler.dotson@gmail.com>
2016-12-19 14:20:34 -0800

    initial commit

--- lab-kyler/server.js
+++ lab-kyler/server.js
@@ -1,17 +1,27 @@
 'use strict';
 
-const http = require('http');
-const Router = require('./lib/router.js');
-const router = new Router();
-// const storage = require('./lib/memStorage.js');
-// const Joke = require('./model/joke.js');
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('joke:server'); //AHHHHHHH wutt
 
+const app = express();
+app.disable('x-powered-by'); //stupid
+
+const Joke = require('./model/joke.js');
+const storage = require('./lib/diskStorage.js');
 const PORT = process.env.PORT || 2001;
 
-const server = http.createServer(router.route());
 
-require('./route/note-route.js')(router);
+app.use(morgan('dev')); //logging happens through morgan
 
-server.listen(PORT, () => {
-  console.log('Server running on port', PORT);
+app.get('/test', function(req, res) {
+  debug('GET: /test'); //leave the debug at the top of the route...
+
+  res.json({'msg': 'test route!'});
+});
+
+app.listen(PORT, () => { //called "lexical error function"
+  console.log(`Server listening on port ${PORT}`);
 });
